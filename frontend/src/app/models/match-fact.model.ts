56ccb035142e92c0b641a90b5edbde98d9148439
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-07-27 21:24:36 +0200

    Added MatchFact model for Angular frontend.

---
+++ frontend/src/app/models/match-fact.model.ts
@@ -0,0 +1,9 @@
+export interface MatchFact {
+  id?: number;
+  player_id: number;
+  kilometers_run: number;
+  sprints: number;
+  intensive_runs: number;
+  distance_run: number;
+  match_date: Date;
+}
