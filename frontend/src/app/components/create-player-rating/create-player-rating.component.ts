ba228aa600601f273b04e01263126af165586fd5
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-07-27 21:30:21 +0200

    Added CreatePlayerRatingComponent for Angular frontend.

---
+++ frontend/src/app/components/create-player-rating/create-player-rating.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { PlayerRatingService } from '../../services/player-rating.service';
+import { PlayerRating } from '../../models/player-rating.model';
+
+@Component({
+  selector: 'app-create-player-rating',
+  templateUrl: './create-player-rating.component.html',
+})
+export class CreatePlayerRatingComponent {
+  playerRating: PlayerRating = {
+    player_id: 0,
+    coach_id: 0,
+    ball_manipulation: 0,
+    kicking_ability: 0,
+    passing_ability: 0,
+    duel_tackling: 0,
+    field_coverage: 0,
+    blocking_ability: 0,
+    game_strategy: 0,
+    playmaking_risk: 0,
+    rating_date: new Date()
+  };
+
+  constructor(private playerRatingService: PlayerRatingService) { }
+
+  createPlayerRating() {
+    this.playerRatingService.createPlayerRating(this.playerRating).subscribe();
+  }
+}
