c97b2caf94d29564bb4fcb4ee4be749c56185f8d
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-07-27 21:41:05 +0200

    Added route guards for CreateMatchFactComponent and CreatePlayerRatingComponent in app-routing.module.ts.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateMatchFactComponent } from './components/create-match-fact/create-match-fact.component';
 import { CreatePlayerRatingComponent } from './components/create-player-rating/create-player-rating.component';
+import { AuthGuard } from './services/auth.guard';
+
 
 const routes: Routes = [
   { path: 'create-match-fact', component: CreateMatchFactComponent },
