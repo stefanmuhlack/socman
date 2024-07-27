e074156993a7bcf7c3e83124491a47883111d033
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-07-27 21:27:14 +0200

    Added CreateMatchFactComponent for Angular frontend.

---
+++ frontend/src/app/components/create-match-fact/create-match-fact.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { MatchFactService } from '../../services/match-fact.service';
+import { MatchFact } from '../../models/match-fact.model';
+
+@Component({
+  selector: 'app-create-match-fact',
+  templateUrl: './create-match-fact.component.html',
+})
+export class CreateMatchFactComponent {
+  matchFact: MatchFact = {
+    player_id: 0,
+    kilometers_run: 0,
+    sprints: 0,
+    intensive_runs: 0,
+    distance_run: 0,
+    match_date: new Date()
+  };
+
+  constructor(private matchFactService: MatchFactService) { }
+
+  createMatchFact() {
+    this.matchFactService.createMatchFact(this.matchFact).subscribe();
+  }
+}
