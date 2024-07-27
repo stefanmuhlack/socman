114d2d80386c210a78d9617e12e22d38ae8335fe
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-07-27 21:20:19 +0200

    Added MatchFact service for Angular frontend.

---
+++ frontend/src/app/services/match-fact.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MatchFact } from './match-fact.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MatchFactService {
+  private apiUrl = 'http://localhost:8000/match-facts/';
+
+  constructor(private http: HttpClient) { }
+
+  createMatchFact(matchFact: MatchFact): Observable<MatchFact> {
+    return this.http.post<MatchFact>(this.apiUrl, matchFact);
+  }
+
+  getMatchFacts(skip: number = 0, limit: number = 10): Observable<MatchFact[]> {
+    return this.http.get<MatchFact[]>(`${this.apiUrl}?skip=${skip}&limit=${limit}`);
+  }
+
+  getMatchFact(matchFactId: number): Observable<MatchFact> {
+    return this.http.get<MatchFact>(`${this.apiUrl}${matchFactId}`);
+  }
+}
