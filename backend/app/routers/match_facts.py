8381cb2a6197241fe1086dfc79ece6439854c640
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-07-29 09:33:54 +0200

    Update match_facts.py
    Added PUT and DELETE routes for Match Fact in match_facts.py.

--- backend/app/routers/match_facts.py
+++ backend/app/routers/match_facts.py
@@ -22,3 +22,14 @@ def read_match_fact(match_fact_id: int, db: Session = Depends(get_db)):
     if db_match_fact is None:
         raise HTTPException(status_code=404, detail="MatchFact not found")
     return db_match_fact
+
+@router.put("/match-facts/{match_fact_id}", response_model=schemas.MatchFact)
+def update_match_fact(match_fact_id: int, match_fact: schemas.MatchFactUpdate, db: Session = Depends(get_db)):
+    return crud.update_match_fact(db=db, match_fact=match_fact)
+
+@router.delete("/match-facts/{match_fact_id}", response_model=schemas.MatchFact)
+def delete_match_fact(match_fact_id: int, db: Session = Depends(get_db)):
+    db_match_fact = crud.delete_match_fact(db=db, match_fact_id=match_fact_id)
+    if db_match_fact is None:
+        raise HTTPException(status_code=404, detail="MatchFact not found")
+    return db_match_fact
