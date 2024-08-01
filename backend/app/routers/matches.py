3f65fd7b78b6a6ec7c4fb14a8c97a1354f9aae66
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-08-01 18:52:10 +0200

    Update matches.py

--- backend/app/routers/matches.py
+++ backend/app/routers/matches.py
@@ -2,14 +2,22 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from .. import crud, models, schemas
 from ..database import SessionLocal, engine
+from ..auth import get_current_admin_or_higher
 from typing import List
 
 router = APIRouter()
 
 models.Base.metadata.create_all(bind=engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @router.post("/matches/", response_model=schemas.Match)
-def create_match(match: schemas.MatchCreate, db: Session = Depends(get_db)):
+def create_match(match: schemas.MatchCreate, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_admin_or_higher)):
     return crud.create_match(db=db, match=match)
 
 @router.get("/matches/", response_model=List[schemas.Match])
@@ -20,6 +28,17 @@ def read_matches(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
 @router.get("/matches/{match_id}", response_model=schemas.Match)
 def read_match(match_id: int, db: Session = Depends(get_db)):
     db_match = crud.get_match(db, match_id)
+    if db_match is None:
+        raise HTTPException(status_code=404, detail="Match not found")
+    return db_match
+
+@router.put("/matches/{match_id}", response_model=schemas.Match)
+def update_match(match_id: int, match: schemas.MatchUpdate, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_admin_or_higher)):
+    return crud.update_match(db=db, match=match)
+
+@router.delete("/matches/{match_id}", response_model=schemas.Match)
+def delete_match(match_id: int, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_admin_or_higher)):
+    db_match = crud.delete_match(db=db, match_id=match_id)
     if db_match is None:
         raise HTTPException(status_code=404, detail="Match not found")
     return db_match
