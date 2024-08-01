e97843fbcb1dbff8ffeb93c6df3b8aee6eb9f378
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-08-01 19:09:49 +0200

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CreateMatchFactComponent } from './components/create-match-fact/create-match-fact.component';
-import { CreatePlayerRatingComponent } from './components/create-player-rating/create-player-rating.component';
-import { AuthGuard } from './services/auth.guard';
+import { PlayerRatingChartComponent } from './components/player-rating-chart/player-rating-chart.component';
 
 const routes: Routes = [
-  { path: 'create-match-fact', component: CreateMatchFactComponent, canActivate: [AuthGuard] },
-  { path: 'create-player-rating', component: CreatePlayerRatingComponent, canActivate: [AuthGuard] },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: 'player-rating/:playerId', component: PlayerRatingChartComponent },
+  // other routes
 ];
 
 @NgModule({
