2778ead8b393fba0e61dacac080bdbbddecea54e
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-09-24 13:35:45 +0200

    Create player-team.service.ts

---
+++ frontend/src/app/services/player-team.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PlayerTeam } from '../models/player-team.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlayerTeamService {
+  private apiUrl = 'http://localhost:8000/player-teams/';
+
+  constructor(private http: HttpClient) {}
+
+  getPlayerTeams(playerId: number): Observable<PlayerTeam[]> {
+    return this.http.get<PlayerTeam[]>(`${this.apiUrl}${playerId}`);
+  }
+
+  addPlayerToTeam(playerTeam: PlayerTeam): Observable<PlayerTeam> {
+    return this.http.post<PlayerTeam>(this.apiUrl, playerTeam);
+  }
+}
