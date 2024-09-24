638dfbb6bd3c8ffb7924440f0136012b3474c47c
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-09-24 13:33:28 +0200

    Create player-team-management.component.ts

---
+++ frontend/src/app/components/player-team-management/player-team-management.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { PlayerTeamService } from '../../services/player-team.service';
+import { PlayerTeam } from '../../models/player-team.model';
+
+@Component({
+  selector: 'app-player-team-management',
+  templateUrl: './player-team-management.component.html',
+})
+export class PlayerTeamManagementComponent implements OnInit {
+  playerTeams: PlayerTeam[] = [];
+  newPlayerTeam: PlayerTeam = {
+    player_id: 0,
+    team_id: 0,
+    role: 'primary',  // Default to primary role
+  };
+
+  constructor(private playerTeamService: PlayerTeamService) {}
+
+  ngOnInit(): void {
+    this.loadPlayerTeams();
+  }
+
+  loadPlayerTeams() {
+    this.playerTeamService.getPlayerTeams(this.newPlayerTeam.player_id).subscribe(
+      (data) => {
+        this.playerTeams = data;
+      },
+      (error) => {
+        // Handle error
+      }
+    );
+  }
+
+  addPlayerToTeam() {
+    this.playerTeamService.addPlayerToTeam(this.newPlayerTeam).subscribe(
+      () => {
+        // Refresh the player-team list after adding
+        this.loadPlayerTeams();
+      },
+      (error) => {
+        // Handle error
+      }
+    );
+  }
+}
