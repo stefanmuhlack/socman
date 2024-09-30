0ff6754a4256bf06149768a9e54755c355ac21a4
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-09-30 14:05:03 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,3 +4,11 @@ from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from . import crud, models, schemas, auth
 from .database import SessionLocal, engine
+from .routers import notifications
+
+app = FastAPI()
+
+# WebSocket for notifications
+app.include_router(notifications.router)
+
+# Other routes
