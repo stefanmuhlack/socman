1360774c3211ef236ab73ae59e4e42f8d6205b36
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-09-30 14:55:47 +0200

    Create create-match-statistics.component.ts

---
+++ frontend/src/app/components/create-match-statistics.component/create-match-statistics.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { MatchStatisticsService } from '../../services/match-statistics.service';
+
+@Component({
+  selector: 'app-create-match-statistics',
+  templateUrl: './create-match-statistics.component.html',
+})
+export class CreateMatchStatisticsComponent {
+  statistics = {
+    match_id: 0,
+    player_id: 0,
+    goals: 0,
+    assists: 0,
+    tackles: 0,
+    passes_completed: 0,
+    fouls_committed: 0,
+    minutes_played: 0,
+    team_id: 0
+  };
+
+  constructor(private matchStatisticsService: MatchStatisticsService) {}
+
+  submitStatistics() {
+    this.matchStatisticsService.submitStatistics(this.statistics).subscribe(
+      () => {
+        // Handle success
+      },
+      (error) => {
+        // Handle error
+      }
+    );
+  }
+}
