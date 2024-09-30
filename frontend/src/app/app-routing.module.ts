cec21d2a89023bc5ed5186fba7158610d3b0bfad
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-09-30 15:04:30 +0200

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,13 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { PlayerRatingChartComponent } from './components/player-rating-chart/player-rating-chart.component';
 
 const routes: Routes = [
-  { path: 'player-rating/:playerId', component: PlayerRatingChartComponent },
-  // other routes
-];
-
-const routes: Routes = [
   { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
-  { path: 'player-rating', loadChildren: () => import('./player-rating/player-rating.module').then(m => m.PlayerRatingModule) },
+  { path: 'player-ratings', loadChildren: () => import('./player-rating/player-rating.module').then(m => m.PlayerRatingModule) },
+  { path: 'tournament-leaderboard', loadChildren: () => import('./tournament-leaderboard/tournament-leaderboard.module').then(m => m.TournamentLeaderboardModule) },
+  { path: 'match-statistics', loadChildren: () => import('./match-statistics/match-statistics.module').then(m => m.MatchStatisticsModule) },
 ];
 
 @NgModule({
