c2855475eaa9c6267d55e5b2b896125c01b6ccbc
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-09-30 13:44:55 +0200

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float, JSON, Boolean
 from sqlalchemy.orm import relationship
 from .database import Base
+from datetime import datetime
 
 class User(Base):
     __tablename__ = 'users'
