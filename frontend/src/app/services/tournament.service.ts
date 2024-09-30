0b7f5946242cde7e93205f0f8395ab0e45e9fec2
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-09-30 16:08:08 +0200

    Update tournament.service.ts

--- frontend/src/app/services/tournament.service.ts
+++ frontend/src/app/services/tournament.service.ts
@@ -1,21 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Tournament } from '../models/tournament.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TournamentService {
   private apiUrl = 'http://localhost:8000/tournaments/';
 
   constructor(private http: HttpClient) {}
 
-  createTournament(tournament: Tournament): Observable<Tournament> {
-    return this.http.post<Tournament>(this.apiUrl, tournament);
+  createTournament(tournament: any): Observable<any> {
+    return this.http.post(this.apiUrl, tournament);
   }
 
-  getTournaments(): Observable<Tournament[]> {
-    return this.http.get<Tournament[]>(this.apiUrl);
+  applyPromotionRelegation(tournamentId: number): Observable<any> {
+    return this.http.post(`${this.apiUrl}/promotion-relegation/`, { tournament_id: tournamentId });
   }
 }
