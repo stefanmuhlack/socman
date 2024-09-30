842441bb3b8d9c5480ef59b8d6b5408cf452cd99
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-09-30 17:05:39 +0200

    Update player-rating.service.ts
    Added missing export function for downloading player data as CSV.

--- frontend/src/app/services/player-rating.service.ts
+++ frontend/src/app/services/player-rating.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { PlayerRatingHistory } from '../models/player-rating-history.model';
 
 @Injectable({
   providedIn: 'root',
@@ -11,14 +10,7 @@ export class PlayerRatingService {
 
   constructor(private http: HttpClient) {}
 
-  getRatingHistory(playerId: number): Observable<PlayerRatingHistory[]> {
-    return this.http.get<PlayerRatingHistory[]>(`${this.apiUrl}${playerId}/ratings/history/`);
+  exportPlayerData(): Observable<Blob> {
+    return this.http.get(`${this.apiUrl}/export`, { responseType: 'blob' });
   }
-}
-exportPlayerData(): Observable<Blob> {
-  return this.http.get('/api/player/export', { responseType: 'blob' });
-}
-
-exportPlayerData(): Observable<Blob> {
-  return this.http.get(`${this.apiUrl}/export`, { responseType: 'blob' });
 }
