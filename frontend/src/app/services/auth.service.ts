2e1df373778580f94c2f2e9758a0b65cb64ad6a5
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-09-30 13:26:01 +0200

    Update auth.service.ts

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,26 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:8000/auth/';
+  constructor(private http: HttpClient) { }
 
-  constructor(private http: HttpClient, public jwtHelper: JwtHelperService) {}
-
-  login(credentials: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}login`, credentials);
+  getUserRole(): string {
+    // Example: Assuming you have the user role stored in local storage after login
+    return localStorage.getItem('userRole') || '';
   }
 
-  register(user: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}register`, user);
-  }
-
-  isAuthenticated(): boolean {
-    const token = localStorage.getItem('token');
-    return !this.jwtHelper.isTokenExpired(token);
-  }
+  // Additional Auth-related methods
 }
