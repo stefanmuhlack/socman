598286bc42ab8f97194ba9a1648d4ab960ec1edd
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-10-01 14:19:56 +0200

    Update app.py

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
-from .routers import notifications
+from .routers import notifications, tactical_formations, players, teams, tournaments, matches
 
 app = FastAPI()
 
 # WebSocket for notifications
 app.include_router(notifications.router)
 
-# Other routes
+# Additional routers
+app.include_router(tactical_formations.router)
+app.include_router(players.router)
+app.include_router(teams.router)
+app.include_router(tournaments.router)
+app.include_router(matches.router)
