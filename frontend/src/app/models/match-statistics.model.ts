c675186568ff0846b51e07d24e9003af0bd18ea7
Sebastian Kiener <sebastian.kiener@wwe-media.eu>
2024-10-09 19:25:32 +0200

    added missing match statistics model and service

---
+++ frontend/src/app/models/match-statistics.model.ts
@@ -0,0 +1,11 @@
+export interface MatchStatistics {
+  match_id: number;
+  player_id: number;
+  goals: number;
+  assists: number;
+  tackles: number;
+  passes_completed: number;
+  fouls_committed: number;
+  minutes_played: number;
+  team_id: number;
+}
---
+++ frontend/src/app/services/match-statistics.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MatchStatistics } from '../models/match-statistics.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MatchStatisticsService {
+  private apiUrl = 'http://localhost:8000/match-statistics/';
+
+  constructor(private http: HttpClient) { }
+
+  createMatchStatistics(MatchStatistics: MatchStatistics): Observable<MatchStatistics> {
+    return this.http.post<MatchStatistics>(this.apiUrl, MatchStatistics);
+  }
+
+  getMatchStatisticss(skip: number = 0, limit: number = 10): Observable<MatchStatistics[]> {
+    return this.http.get<MatchStatistics[]>(`${this.apiUrl}?skip=${skip}&limit=${limit}`);
+  }
+
+  getMatchStatistics(MatchStatisticsId: number): Observable<MatchStatistics> {
+    return this.http.get<MatchStatistics>(`${this.apiUrl}${MatchStatisticsId}`);
+  }
+
+  submitStatistics(MatchStatistics: MatchStatistics): Observable<MatchStatistics> {
+    return this.http.patch<MatchStatistics>(`${this.apiUrl}${MatchStatistics.id}`, MatchStatistics);
+  }
+}
