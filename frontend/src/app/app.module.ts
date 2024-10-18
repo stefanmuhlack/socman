320332291b570b538ce109de409601f5c7cac90d
Sebastian Kiener <sebastian.kiener@wwe-media.eu>
2024-10-18 11:09:14 +0200

    added forms module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PlayerRatingChartComponent } from './components/player-rating-chart/player-rating-chart.component';
 import { ChartsModule } from 'ng2-charts';
@@ -14,7 +15,7 @@ import { ChartsModule } from 'ng2-charts';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ChartsModule,
+    FormsModule,
     // other modules
   ],
   providers: [],
